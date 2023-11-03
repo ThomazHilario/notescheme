6db13ff6330c99cf1f8d6e7869a32271257ca4ba
Thomaz Alves <thomazhilario5@gmail.com>
2023-11-03 17:50:57 -0300

    Iniciando projeto

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import RoutePage from './pages/routes';
+
+function App() {
+  return (
+    <div className="App">
+      <RoutePage/>
+    </div>
+  );
+}
+
+export default App;
