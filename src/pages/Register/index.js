d7a364b482f969c501e9a30e1b430c27e205aa2f
Thomaz Alves <thomazhilario5@gmail.com>
2023-12-26 12:38:05 -0300

    Criando um componente de Registro

---
+++ src/pages/Register/index.js
@@ -0,0 +1,70 @@
+import {useState} from 'react'
+import { Navigate } from 'react-router-dom'
+import { database, auth } from '../../services'
+import { setDoc, doc } from 'firebase/firestore'
+import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { toast } from 'react-toastify'
+
+export default function Register(){
+
+    // states - input
+    const [email,setEmail] = useState('')
+    const [password,setPassword] = useState('')
+
+
+    // sing Up User
+    async function singUpUser(e){
+        // cancelando formulario
+        e.preventDefault()
+        
+        try {
+            // Criando usuario
+            const users = await createUserWithEmailAndPassword(auth,email,password)
+
+            // Criando banco de dados deste usuario
+            await setDoc(doc(database, "Login-Users", users.user.uid), {
+                myNotes:[]
+            });
+
+            // navegando ate a pasta
+            <Navigate to={`/admin/${users.user.uid}`} replace={true}/>
+
+            // Alerta de sucesso
+            toast.success('Usuario Cadastrado')
+
+        } catch (error) {
+            // Caso o email ja esteja registrado.
+            if(error.code === 'auth/email-already-in-use'){
+                toast.error('Este email ja foi registrado!')
+            } else{
+                // Caso a senha tenha menos de 6 caracteres.
+                toast.warn('A sua senha deve conter ate 6 caracteres!!')
+            }
+        }
+
+
+    }
+
+    return(
+        <form>
+                <legend className="titulo">CADASTRO</legend>
+                
+                {/* name */}
+                <div className="container_input">
+                    <label>Email:</label>
+                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                </div>
+
+                {/* Password */}
+                <div className="container_input">
+                    <label>Password:</label>
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                </div>
+
+                {/* Buttons */}
+                <div className="container_button">
+                    <button onClick={singUpUser}>Cadastrar</button>
+                </div>
+            </form>
+    )
+}
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,9 +2,8 @@ import { useState, useEffect } from "react";
 import './home.css'
 import {useNavigate} from 'react-router-dom'
 import {toast} from 'react-toastify'
-import {database, auth} from "../../services";
-import {createUserWithEmailAndPassword, signInWithEmailAndPassword,onAuthStateChanged} from 'firebase/auth'
-import { doc, setDoc } from "firebase/firestore";
+import {auth} from "../../services";
+import {signInWithEmailAndPassword,onAuthStateChanged} from 'firebase/auth'
 
 export default function Home(){
 
@@ -34,53 +33,6 @@ export default function Home(){
     const [email,setEmail] = useState('')
     const [password,setPassword] = useState('')
 
-    // states - rederização
-    const [pagina,setPagina] = useState(true)
-
-    // Mudando o valor da state para false
-    function mudarValor(e){
-        // cancelando formulario
-        e.preventDefault()
-
-        // Mudando o valor da state para false
-        if(pagina === true){
-            setPagina(false)
-        } else{
-            setPagina(true)
-        }
-    }
-
-    // sing Up User
-    async function singUpUser(e){
-        // cancelando formulario
-        e.preventDefault()
-        try {
-            // Criando usuario
-            const users = await createUserWithEmailAndPassword(auth,email,password)
-
-            // Criando banco de dados deste usuario
-            await setDoc(doc(database, "Login-Users", users.user.uid), {
-                myNotes:[]
-            });
-
-            // navegando ate a pasta
-            navigate(`/admin/${users.user.uid}`)
-
-            // Alerta de sucesso
-            toast.success('Usuario Cadastrado')
-
-        } catch (error) {
-            // Caso o email ja esteja registrado.
-            if(error.code === 'auth/email-already-in-use'){
-                toast.error('Este email ja foi registrado!')
-            } else{
-                // Caso a senha tenha menos de 6 caracteres.
-                toast.warn('A sua senha deve conter ate 6 caracteres!!')
-            }
-        }
-
-
-    }
 
     // sing in User
     async function singInUser(e){
@@ -101,32 +53,6 @@ export default function Home(){
         }
     }
 
-    // Condição de renderização
-    if(pagina === true){
-        return(
-            <form>
-                <legend className="titulo">CADASTRO</legend>
-                
-                {/* name */}
-                <div className="container_input">
-                    <label>Email:</label>
-                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
-                </div>
-
-                {/* Password */}
-                <div className="container_input">
-                    <label>Password:</label>
-                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
-                </div>
-
-                {/* Buttons */}
-                <div className="container_button">
-                    <button onClick={singUpUser}>Cadastrar</button>
-                    <button onClick={mudarValor}>Fazer login</button>
-                </div>
-            </form>
-        )
-    }else{
         return(
             <form>
                 <legend className="titulo">Login</legend>
@@ -146,9 +72,7 @@ export default function Home(){
                 {/* Buttons */}
                 <div className="container_button">
                     <button onClick={singInUser}>Entrar</button>
-                    <button onClick={mudarValor}>Fazer Cadastro</button>
                 </div>
             </form>
         )
-    }
 }
